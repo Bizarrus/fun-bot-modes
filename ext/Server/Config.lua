4da8376bd70ee38cc713d93253d86bd447a57031
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-04-28 15:17:02 +0200

    Create Config.lua

---
+++ ext/Server/Config.lua
@@ -0,0 +1,18 @@
+--
+-- Configuration
+-- <GameMode> = <BotSize>
+--
+
+Config = {
+	TeamDeathMatchC0		= 20,
+	TeamDeathMatch0			= 10,
+	GunMaster0				= 5,
+	Scavenger0				= 10,
+	ConquestLarge0			= 30,
+	ConquestSmall0			= 15,
+	ConquestAssaultLarge0	= 20,
+	ConquestAssaultSmall0	= 15,
+	ConquestAssaultSmall1	= 10,
+	BFLAG					= 15,
+	RushLarge0				= 20
+};
