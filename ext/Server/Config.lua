3cd02f176bb3856fa70e86bfd6268f208bf3b302
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-04-29 15:09:03 +0200

    Adding unavailable modes

--- ext/Server/Config.lua
+++ ext/Server/Config.lua
@@ -7,32 +7,36 @@
 
 Config = {
 	-- Conquest
-	ConquestLarge0			= 30,
-	ConquestSmall0			= 15,
+	ConquestLarge0		= 30,
+	ConquestSmall0		= 15,
 	ConquestAssaultLarge0	= 20,
 	ConquestAssaultSmall0	= 15,
 	ConquestAssaultSmall1	= 10,
 	
 	-- Rush
-	RushLarge0				= 20,
+	RushLarge0		= 20,
 	
 	-- Squad-Modes
-	SquadRush0				= { 5, 5, 5, 5 },
-	SquadDeathMatch0		= { 5, 5, 5, 5 },
+	SquadRush0		= { 5, 5, 5, 5 },
+	SquadDeathMatch0	= { 5, 5, 5, 5 },
 	
 	-- Team-Deathmatch
-	TeamDeathMatch0			= 20,
-	TeamDeathMatchC0		= 20,
+	TeamDeathMatch0		= 20,
+	TeamDeathMatchC0	= 20,
 	
 	-- Domination
-	Domination0				= 10,
+	Domination0		= 10,
 	
 	-- Gunmaster
-	GunMaster0				= 10,
+	GunMaster0		= 10,
 	
 	-- Scavenger
-	Scavenger0				= 10,
+	Scavenger0		= 10,
 	
 	-- Capture the Flag
-	CaptureTheFlag0			= 15
+	CaptureTheFlag0		= 15,
+	
+	-- Currently unavailable on fun-bots:
+	TankSuperiority0	= 10,
+	AirSuperiority0		= 10
 };
